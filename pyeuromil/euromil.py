ca8a4f75f5831c84ed34235705113376a417798a
acpirience <acpirience@gmail.com>
2018-10-18 13:09:07 +0200

    initial work on storage

--- pyeuromil/euromil.py
+++ pyeuromil/euromil.py
@@ -1,13 +1,27 @@
 """ A python library to check and analyse Euromillions results """
 import pkg_resources
 
+
 class Euromil:
     """ Main class for pyeuromil"""
+
     def __init__(self):
+        self._storage = {}
+
+    def _load_data(self, year):
+        """ Load data in storage per year """
+        key = str(year)
+        self._storage[key] = {}
         resource_package = __name__
-        resource_path = '/'.join(('data', '2011.txt'))
+        resource_path = "/".join(("data", key + ".txt"))
+
         with pkg_resources.resource_stream(resource_package, resource_path) as data:
-            self.test = data.readline().strip().decode("utf-8")
+
+            for line in data.readlines():
+                line = line.strip().decode("utf-8").split(" ")
+                if line[0] != "date":
+                    self._storage[key][line[0]] = line[1:]
+
 
 if __name__ == "__main__":
     pass
--- tests/test_euromil.py
+++ tests/test_euromil.py
@@ -2,7 +2,11 @@
 from pyeuromil import Euromil
 
 
-def test_euromil():
-    """ simple read test """
+def test_euromil_load_data():
+    """ data load test """
     my_euromil = Euromil()
-    assert my_euromil.test == "date       n1 n2 n3 n4 n5 *1 *2"
+    my_euromil._load_data(2011)
+    my_euromil._load_data(2012)
+
+    assert my_euromil._storage["2011"]["30/12/2011"], "16 36 43 44 50 07 08"
+    assert my_euromil._storage["2012"]["03/01/2012"], "03 30 42 45 49 05 10"
