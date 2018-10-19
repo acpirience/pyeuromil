85698818f36b85c8cb89e15b8a2606b99d3c807e
acpirience <acpirience@gmail.com>
2018-10-19 13:18:02 +0200

    add access method to values

--- pyeuromil/euromil.py
+++ pyeuromil/euromil.py
@@ -1,5 +1,11 @@
 """ A python library to check and analyse Euromillions results """
+from collections import namedtuple
 import pkg_resources
+
+
+Result = namedtuple("Result", ["date", "n1", "n2", "n3", "n4", "n5", "star1", "star2"])
+MIN_YEAR = 2011
+MAX_YEAR = 2018
 
 
 class Euromil:
@@ -16,11 +22,42 @@ class Euromil:
         resource_path = "/".join(("data", key + ".txt"))
 
         with pkg_resources.resource_stream(resource_package, resource_path) as data:
-
+            data.readline()
             for line in data.readlines():
-                line = line.strip().decode("utf-8").split(" ")
-                if line[0] != "date":
-                    self._storage[key][line[0]] = line[1:]
+                result = Result(*(line.strip().decode("utf-8").split(" ")))
+                self._storage[key][result.date] = result
+
+    def results(self, year, month=None, day=None):
+        """ get a result list from a date """
+        if isinstance(year, int) and year >= MIN_YEAR and year <= MAX_YEAR:
+            # lazy load data values if not already loaded in memory
+            if str(year) not in self._storage:
+                self._load_data(year)
+
+            if month is None and day is None:
+                return self._storage[str(year)]
+
+            if isinstance(month, int) and month >= 1 and month <= 12:
+                if day is None:
+                    month_result = {}
+                    for result in self._storage[str(year)]:
+                        if int(result[3:5]) == month:
+                            month_result[result] = self._storage[str(year)][result]
+                    return month_result
+
+                if isinstance(day, int):
+                    date = f"{day:02}/{month:02}/{year}"
+                    if date in self._storage[str(year)]:
+                        return self._storage[str(year)][date]
+                ValueError(f"Day must be an int")
+
+            ValueError(f"Month must be an int between 1 and 12")
+
+        raise ValueError(f"Year must be an int between {MIN_YEAR} and {MAX_YEAR}")
+
+    def dummy2(self):
+        """ dummy method 2 """
+        pass
 
 
 if __name__ == "__main__":
--- tests/test_euromil.py
+++ tests/test_euromil.py
@@ -1,12 +1,76 @@
 """ Unit tests for Euromil.py """
+import pytest
 from pyeuromil import Euromil
+
+
+def test_euromil_init():
+    """ __init test_ """
+    my_euromil = Euromil()
+    assert my_euromil._storage == {}
 
 
 def test_euromil_load_data():
     """ data load test """
     my_euromil = Euromil()
-    my_euromil._load_data(2011)
-    my_euromil._load_data(2012)
 
-    assert my_euromil._storage["2011"]["30/12/2011"], "16 36 43 44 50 07 08"
-    assert my_euromil._storage["2012"]["03/01/2012"], "03 30 42 45 49 05 10"
+    my_euromil._load_data(2011)
+    assert my_euromil._storage["2011"]
+    assert my_euromil._storage["2011"]["30/12/2011"].n1 == "16"
+
+    my_euromil._load_data(2012)
+    assert my_euromil._storage["2012"]
+    assert my_euromil._storage["2012"]["03/01/2012"].star2 == "10"
+
+
+def test_euromil_results_year_not_exist():
+    """ results of year test (year does not exists)"""
+    my_euromil = Euromil()
+    with pytest.raises(ValueError):
+        results = my_euromil.results("abcd")
+        assert results is None
+        results = my_euromil.results(1920)
+        assert results is None
+
+
+def test_euromil_results_year_exist():
+    """ results of year test (year exists)"""
+    my_euromil = Euromil()
+    results = my_euromil.results(2011)
+    assert results["30/12/2011"].n1 == "16"
+
+
+def test_euromil_results_year_month_not_exist():
+    """ results of year+month test (month does not exists)"""
+    my_euromil = Euromil()
+    with pytest.raises(ValueError):
+        results = my_euromil.results(2011, 13)
+        assert results is None
+        results = my_euromil.results(2011, "abcd")
+        assert results is None
+
+
+def test_euromil_results_year_month_exist():
+    """ results of year+month test (month exists)"""
+    my_euromil = Euromil()
+    results = my_euromil.results(2011, 10)
+    assert results["14/10/2011"].n1 == "12"
+    assert results["04/10/2011"].star1 == "08"
+
+
+def test_euromil_results_year_month_day_not_exist():
+    """ results of year+month+day test (day not exist)"""
+    my_euromil = Euromil()
+    with pytest.raises(ValueError):
+        results = my_euromil.results(2011, 10, 5)
+        assert results is None
+        results = my_euromil.results(2011, 10, "abcd")
+        assert results is None
+
+
+def test_euromil_results_year_month_day_exist():
+    """ results of year+month+day test (all exist)"""
+    my_euromil = Euromil()
+    results = my_euromil.results(2011, 10, 4)
+    assert results.n1 == "14"
+    results = my_euromil.results(2011, 10, 14)
+    assert results.star1 == "03"
