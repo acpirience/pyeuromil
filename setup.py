6a2947b0b9c9d63a0648585c1bfe6c59190b0839
acpirience <acpirience@gmail.com>
2018-10-19 16:34:19 +0200

    bump version to 0.1.1 => upload to pypi

--- setup.py
+++ setup.py
@@ -15,7 +15,7 @@ def read(filename):
 
 setup(
     name="pyeuromil",
-    version="0.1.0",
+    version="0.1.1",
     url="https://github.com/acpirience/pyeuromil",
     author="Acpirience",
     author_email="acpirience@gmail.com",
