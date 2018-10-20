3b710bfba742ded3176cc6e57b521d03ba56b06a
acpirience <acpirience@gmail.com>
2018-10-20 12:33:52 +0200

    update version for pypi publication

--- setup.py
+++ setup.py
@@ -15,7 +15,7 @@ def read(filename):
 
 setup(
     name="pyeuromil",
-    version="0.1.1",
+    version="0.1.2",
     url="https://github.com/acpirience/pyeuromil",
     author="Acpirience",
     author_email="acpirience@gmail.com",
