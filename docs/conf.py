0e53c367500dc2fdedd68de4157fe70df9a8aa2a
acpirience <acpirience@gmail.com>
2018-11-17 07:09:16 +0100

    bump version to 0.1.20181116

--- docs/conf.py
+++ docs/conf.py
@@ -24,7 +24,7 @@ copyright = "2018, Acpirience"
 author = "Acpirience"
 
 # The short X.Y version
-version = "0.1.20181106"
+version = "0.1.20181116"
 # The full version, including alpha/beta/rc tags
 release = "beta"
 
--- setup.py
+++ setup.py
@@ -16,7 +16,7 @@ def read(filename):
 
 setup(
     name="pyeuromil",
-    version="0.1.20181106",
+    version="0.1.20181116",
     url="https://github.com/acpirience/pyeuromil",
     project_urls=OrderedDict(
         (
