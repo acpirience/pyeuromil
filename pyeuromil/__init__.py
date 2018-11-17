1c2d9002d3285628b6eef42726f665e221edb03a
acpirience <acpirience@gmail.com>
2018-11-17 07:08:20 +0100

    Last results = 2018/11/16

--- pyeuromil/__init__.py
+++ pyeuromil/__init__.py
@@ -1,7 +1,7 @@
 """pyeuromil - A python library to check and analyse Euromillions results"""
 
 
-__version__ = "0.1.20181106"
+__version__ = "0.1.20181116"
 __author__ = "Acpirience <acpirience@gmail.com>"
 __all__ = []
 
