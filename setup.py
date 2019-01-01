3c4411923ad78e7541df7d34e1203d8e2f6aad10
acpirience <acpirience@gmail.com>
2019-01-01 08:20:21 +0100

    bump version to 0.2.20190101

--- setup.py
+++ setup.py
@@ -16,7 +16,7 @@ def read(filename):
 
 setup(
     name="pyeuromil",
-    version="0.2.20181221",
+    version="0.2.20190101",
     url="https://github.com/acpirience/pyeuromil",
     project_urls=OrderedDict(
         (
