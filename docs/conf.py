c7c4ae16c88c0c157af6fd6d3df38dc5e9102bb3
acpirience <acpirience@gmail.com>
2019-02-09 08:25:43 +0100

    Last results = 2019/02/08

--- docs/conf.py
+++ docs/conf.py
@@ -24,7 +24,7 @@ copyright = "2018, Acpirience"
 author = "Acpirience"
 
 # The short X.Y version
-version = "0.2.20190125"
+version = "0.2.20190208"
 # The full version, including alpha/beta/rc tags
 release = "beta"
 
