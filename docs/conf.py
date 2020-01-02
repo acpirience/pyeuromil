1de5146e1dd508a8e2f5072522174aa72057418d
acpirience <acpirience@gmail.com>
2020-01-02 13:39:48 +0100

    Prepare for last version of the year

--- docs/conf.py
+++ docs/conf.py
@@ -24,7 +24,7 @@ copyright = "2018, Acpirience"
 author = "Acpirience"
 
 # The short X.Y version
-version = "0.2.20190927"
+version = "0.2.20191231"
 # The full version, including alpha/beta/rc tags
 release = "beta"
 
